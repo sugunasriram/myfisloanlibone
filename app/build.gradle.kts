1fe4686ccc8060bbb8aee8c7fe9a315080565a21
Suguna Sriram <haisugusri@gmail.com>
2025-02-10 14:50:47 +0530

    Added AndroidManifest.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,6 @@ dependencies {
     // Compose Navigation
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
-    //implementation("com.github.sugunasriram:myfisloanlibone:v1.0.10")
+    //implementation("com.github.sugunasriram:myfisloanlibone:v1.0.11")
 
 }
