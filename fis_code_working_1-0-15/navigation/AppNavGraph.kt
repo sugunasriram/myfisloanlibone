df116bd252aa31c8efec8a63026e0e4c22c2f6ca
Suguna Sriram <haisugusri@gmail.com>
2025-04-28 14:53:05 +0530

    Deleted folder fis_code_working_1-0-15

--- fis_code_working_1-0-15/navigation/AppNavGraph.kt
+++
@@ -1,6 +0,0 @@
-package com.github.sugunasriram.myfisloanlibone.fis_code.navigation
-
-object AppNavGraph {
-    const val BUYERAPP = "buyer_app_graph"
-    const val GRAPH_LAUNCH = "graph_launch"
-}
--- fis_code_working_1-0-15/viewModel/BaseViewModel.kt
+++
@@ -1,5 +0,0 @@
-package com.github.sugunasriram.myfisloanlibone.fis_code.viewModel
-
-import androidx.lifecycle.ViewModel
-
-open class BaseViewModel : ViewModel() {}
